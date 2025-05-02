8343d7188921d5bf95873710ae27d1457c2a84d5
vladi <vladi@aresgate.net>
2025-05-02 13:49:11 -0700

    skip more tests

--- pkgs/by-name/li/lighthouse/default.nix
+++ pkgs/by-name/li/lighthouse/default.nix
@@ -37,36 +37,29 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-0nClqRSLwKnTNAMsvX5zzN2PbVJ51xtQv48cHSqHLAY=";
   };
 
-  cargoLock = {
-    lockFile = "${src}/Cargo.lock";
-    outputHashes = {
-      "quick-protobuf-0.8.1" = "sha256-dgePLYCeoEZz5DGaLifhf3gEIPaL7XB0QT9wRKY8LJg=";
-      "libmdbx-0.1.4" = "sha256-ONp4uPkVCN84MObjXorCZuSjnM6uFSMXK1vdJiX074o=";
-      "lmdb-rkv-0.14.0" = "sha256-sxmguwqqcyOlfXOZogVz1OLxfJPo+Q0+UjkROkbbOCk=";
-      "xdelta3-0.1.5" = "sha256-aewSexOZCrQoKZQa+SGP8i6JKXstaxF3W2LVEhCSmPs=";
-    };
-  };
+  patches = [
+    ./use-system-sqlite.patch
+  ];
+
+  cargoHash = "sha256-Opkz3EVKw0M4LeWMsn1NlSw/Fg7cWVqnDJRRTPYYlLo=";
+  useFetchCargoVendor = true;
 
   enableParallelBuilding = true;
 
   cargoBuildFlags = [ "--package lighthouse" ];
 
-  nativeBuildInputs = [ cmake clang ];
-  buildInputs = [ openssl protobuf sqlite rust-jemalloc-sys ];
+  nativeBuildInputs = [
+    rustPlatform.bindgenHook
+    cmake
+    pkg-config
+    protobuf
+  ];
 
-  buildNoDefaultFeatures = true;
+  buildInputs = [ rust-jemalloc-sys sqlite ];
+
   buildFeatures = [ "modern" "slasher-lmdb" ];
 
-  # Needed to get openssl-sys to use pkg-config.
-  OPENSSL_NO_VENDOR = 1;
-  OPENSSL_LIB_DIR = "${lib.getLib openssl}/lib";
-  OPENSSL_DIR = "${lib.getDev openssl}";
-
-  # Needed to get prost-build to use protobuf
-  PROTOC = "${protobuf}/bin/protoc";
-
-  # Needed by libmdx
-  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+  OPENSSL_NO_VENDOR = true;
 
   # common crate tries to fetch the compiled version from an URL
   # see: https://github.com/sigp/lighthouse/blob/stable/common/deposit_contract/build.rs#L30
@@ -75,6 +68,15 @@ rustPlatform.buildRustPackage rec {
   # common crate tries to fetch the compiled version from an URL
   # see: https://github.com/sigp/lighthouse/blob/stable/common/deposit_contract/build.rs#L33
   LIGHTHOUSE_DEPOSIT_CONTRACT_TESTNET_URL = "file:${slasherContractTestnetSrc}";
+
+  passthru = {
+    tests.version = testers.testVersion {
+      package = lighthouse;
+      command = "lighthouse --version";
+      version = "v${lighthouse.version}";
+    };
+    updateScript = nix-update-script { };
+  };
 
   # This is needed by the unit tests.
   FORK_NAME = "capella";
@@ -90,56 +92,13 @@ rustPlatform.buildRustPackage rec {
     "--exclude slashing_protection"
     "--exclude watch"
     "--exclude web3signer_tests"
-    "--exclude lighthouse_metrics"
-    "--exclude invalid_attestation_delayed_slot"
-    "--exclude can_read_finalized_block"
-    "--exclude invalid_attestation_future_block"
-    "--exclude invalid_attestation_empty_bitfield"
-    "--exclude invalid_attestation_inconsistent_ffg_vote"
-    "--exclude invalid_attestation_past_epoch"
-    "--exclude invalid_attestation_future_epoch"
-    "--exclude invalid_attestation_target_epoch"
-    "--exclude invalid_block_finalized_slot"
-    "--exclude invalid_block_finalized_descendant"
-    "--exclude invalid_attestation_unknown_target_root"
-    "--exclude invalid_attestation_unknown_beacon_block_root"
-    "--exclude invalid_block_unknown_parent"
-    "--exclude justified_balances"
-    "--exclude justified_and_finalized_blocks"
-    "--exclude invalid_block_future_slot"
-    "--exclude progressive_balances_cache_attester_slashing"
-    "--exclude justified_checkpoint_updates_with_descendent_first_justification"
-    "--exclude justified_checkpoint_updates_with_descendent"
-    "--exclude justified_checkpoint_updates_with_non_descendent"
-    "--exclude valid_attestation"
-    "--exclude valid_attestation_skip_across_epoch"
-    "--exclude weak_subjectivity_check_epoch_boundary_is_skip_slot"
-    "--exclude progressive_balances_cache_proposer_slashing"
-    "--exclude weak_subjectivity_check_fails_late_epoch"
-    "--exclude weak_subjectivity_check_fails_early_epoch"
-    "--exclude weak_subjectivity_check_fails_incorrect_root"
-    "--exclude weak_subjectivity_check_epoch_boundary_is_skip_slot_failure"
-    "--exclude weak_subjectivity_check_passes"
-    "--exclude weak_subjectivity_fail_on_startup"
-    "--exclude weak_subjectivity_pass_on_startup"
-    "--exclude basic"
-  ];
-
-  nativeCheckInputs = [
-    postgresql
-    foundry
   ];
 
   checkFeatures = [ ];
 
   # All of these tests require network access
   checkFlags = [
-    "--skip service::tests::tests::test_dht_persistence"
-    "--skip time::test::test_reinsertion_updates_timeout"
-    "--skip tests::broadcast_should_send_to_all_bns"
-    "--skip tests::check_candidate_order"
-    "--skip tests::first_success_should_try_nodes_in_order"
-    "--skip tests::update_all_candidates_should_update_sync_status"
+    "--skip basic"
     "--skip deposit_tree::cache_consistency"
     "--skip deposit_tree::double_update"
     "--skip deposit_tree::updating"
@@ -150,88 +109,8 @@ rustPlatform.buildRustPackage rec {
     "--skip fast::deposit_cache_query"
     "--skip http::incrementing_deposits"
     "--skip persist::test_persist_caches"
-    "--skip engine_api::http::test::forkchoice_updated_v1_request"
-    "--skip engine_api::http::test::forkchoice_updated_v1_with_payload_attributes_request"
-    "--skip engine_api::http::test::get_block_by_hash_request"
-    "--skip engine_api::http::test::get_block_by_number_request"
-    "--skip engine_api::http::test::get_payload_v1_request"
-    "--skip engine_api::http::test::geth_test_vectors"
-    "--skip engine_api::http::test::new_payload_v1_request"
-    "--skip test::finds_valid_terminal_block_hash"
-    "--skip test::produce_three_valid_pos_execution_blocks"
-    "--skip test::rejects_invalid_terminal_block_hash"
-    "--skip test::rejects_terminal_block_with_equal_timestamp"
-    "--skip test::rejects_unknown_terminal_block_hash"
-    "--skip test::test_forked_terminal_block"
-    "--skip test::verifies_valid_terminal_block_hash"
-    "--skip invalid_attestation_unknown_target_root"
-    "--skip invalid_attestation_unknown_beacon_block_root"
-    "--skip invalid_block_unknown_parent"
-    "--skip justified_balances"
-    "--skip justified_and_finalized_blocks"
-    "--skip invalid_block_future_slot"
-    "--skip progressive_balances_cache_attester_slashing"
-    "--skip justified_checkpoint_updates_with_descendent_first_justification"
-    "--skip justified_checkpoint_updates_with_descendent"
-    "--skip justified_checkpoint_updates_with_non_descendent"
-    "--skip valid_attestation"
-    "--skip valid_attestation_skip_across_epoch"
-    "--skip weak_subjectivity_check_epoch_boundary_is_skip_slot"
-    "--skip progressive_balances_cache_proposer_slashing"
-    "--skip weak_subjectivity_check_fails_late_epoch"
-    "--skip weak_subjectivity_check_fails_early_epoch"
-    "--skip weak_subjectivity_check_fails_incorrect_root"
-    "--skip weak_subjectivity_check_epoch_boundary_is_skip_slot_failure"
-    "--skip weak_subjectivity_check_passes"
-    "--skip weak_subjectivity_fail_on_startup"
-    "--skip weak_subjectivity_pass_on_startup"
-    "--skip can_read_finalized_block"
-    "--skip invalid_block_finalized_descendant"
-    "--skip invalid_block_finalized_slot"
-    "--skip basic"
-    "--skip returns_200_ok"
-    "--skip release_tests::attestation_aggregation_insert_get_prune"
-    "--skip release_tests::attestation_duplicate"
-    "--skip release_tests::attestation_get_max"
-    "--skip release_tests::attestation_pairwise_overlapping"
-    "--skip release_tests::attestation_rewards"
-    "--skip release_tests::cross_fork_attester_slashings"
-    "--skip release_tests::cross_fork_exits"
-    "--skip release_tests::cross_fork_proposer_slashings"
-    "--skip release_tests::duplicate_proposer_slashing"
-    "--skip release_tests::max_coverage_attester_proposer_slashings"
-    "--skip release_tests::max_coverage_different_indices_set"
-    "--skip release_tests::max_coverage_effective_balances"
-    "--skip release_tests::overlapping_max_cover_attester_slashing"
-    "--skip release_tests::prune_attester_slashing_noop"
-    "--skip release_tests::prune_proposer_slashing_noop"
-    "--skip release_tests::simple_max_cover_attester_slashing"
-    "--skip release_tests::sync_contribution_aggregation_insert_get_prune"
-    "--skip release_tests::sync_contribution_duplicate"
-    "--skip release_tests::sync_contribution_with_fewer_bits"
-    "--skip release_tests::sync_contribution_with_more_bits"
-    "--skip release_tests::test_earliest_attestation"
-    "--skip per_block_processing::tests::invalid_bad_proposal_2_signature"
-    "--skip per_block_processing::tests::invalid_block_header_state_slot"
-    "--skip per_block_processing::tests::invalid_block_signature"
-    "--skip per_block_processing::tests::invalid_deposit_bad_merkle_proof"
-    "--skip per_block_processing::tests::invalid_deposit_count_too_small"
-    "--skip per_block_processing::tests::invalid_deposit_deposit_count_too_big"
-    "--skip per_block_processing::tests::invalid_deposit_invalid_pub_key"
-    "--skip per_block_processing::tests::invalid_deposit_wrong_sig"
-    "--skip per_block_processing::tests::invalid_parent_block_root"
-    "--skip per_block_processing::tests::invalid_proposer_slashing_duplicate_slashing"
-    "--skip per_block_processing::tests::invalid_proposer_slashing_proposal_epoch_mismatch"
-    "--skip per_block_processing::tests::invalid_proposer_slashing_proposals_identical"
-    "--skip per_block_processing::tests::invalid_proposer_slashing_proposer_unknown"
-    "--skip per_block_processing::tests::invalid_randao_reveal_signature"
-    "--skip per_block_processing::tests::valid_4_deposits"
-    "--skip per_block_processing::tests::valid_block_ok"
-    "--skip per_block_processing::tests::valid_insert_attester_slashing"
-    "--skip per_block_processing::tests::valid_insert_proposer_slashing"
-    "--skip per_epoch_processing::tests::release_tests::altair_state_on_base_fork"
-    "--skip per_epoch_processing::tests::release_tests::base_state_on_altair_fork"
-    "--skip per_epoch_processing::tests::runs_without_error"
+    "--skip service::tests::tests::test_dht_persistence"
+    "--skip time::test::test_reinsertion_updates_timeout"
   ];
 
   meta = {
